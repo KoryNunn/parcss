d1085aa57e6ab3a9ebc421877788a23f4d8dd801
KoryNunn <knunn187@gmail.com>
2014-08-07 10:32:40 +1000

    Added compress fn

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
+var lex = require('./lexer'),
+    parse = require('./parser'),
+    optimise = require('./optimiser'),
+    render = require('./renderer');
+
 module.exports = {
-    lex: require('./lexer'),
-    parse: require('./parser'),
-    optimise: require('./optimiser'),
-    render: require('./renderer')
+    lex: lex,
+    parse: parse,
+    optimise: optimise,
+    render: render,
+    compress: function(css){
+        return render(optimise(parse(lex(css))));
+    }
 };
